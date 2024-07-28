1ed13ea7f967fb2716b3e69cdf8a5141c4cc5fbf
TALIS_DEV <wordbit.bool@gmail.com>
2024-07-28 12:50:26 -0300

    chore: update shaden theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body className={inter.className}>
         <h1>em comun para todas as paginas</h1>
         {children}</body>
