aac51caaac59737fd56c38c267111e235265092d
TALIS_DEV <wordbit.bool@gmail.com>
2024-08-15 19:27:29 -0300

    feat:add sign in with google

--- app/_components/sidebar-Sheet.tsx
+++ app/_components/sidebar-Sheet.tsx
@@ -1,46 +1,75 @@
+"use client"
+
 import Image from "next/image";
 import { Button } from "./ui/button";
-import { CalendarIcon, HomeIcon, LogOutIcon, } from "lucide-react";
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon, } from "lucide-react";
 import { SheetClose, SheetContent, SheetHeader, SheetTitle} from "./ui/sheet";
 import { quickSearchOptions } from "../_constants/search";
 import { Avatar, AvatarImage  } from "./ui/avatar";
 import Link from "next/link";
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog";
 import { FcGoogle } from "react-icons/fc"
+import { signIn, signOut, useSession } from "next-auth/react";
 
 
 const SidebarSheet = () => {
+    const { data } = useSession();
+    const handleLogoutClick = () => signOut();
+    const hendleLoginWithGoogleClick = async () => {
+       await signIn("google")
+    }
     return ( 
 
         <SheetContent className="overflow-y-auto">
             <SheetHeader>
                 <SheetTitle className="text-left">Menu</SheetTitle>
             </SheetHeader>
-            <div className="border-b border-solid flex items-center justify-between">
+            <div className="py-5 border-b border-solid flex items-center justify-between">
+
+
+                {data?.user ? ( 
+                <div className="flex items-center gap-3">
+                <Avatar className="h-12 w-12 ">
+                 <AvatarImage src={data?.user?.image ?? ""}/>
+               </Avatar>
+               <div>
+               <p className="font-bold text-x ">{data?.user?.name}</p>
+               <p className="text-sm text-gray-400">{data?.user?.email}</p>
+               </div>
+
+               </div>
+                ):(
+                <>
             <Avatar className="h-12 w-12 ">
                     <AvatarImage src="https://cdn-icons-png.flaticon.com/512/4042/4042356.png"/>
                 </Avatar>
 
                 <div>
-                  <p className="pr-9 font-bold text-x ">Olá, faça seu login!</p>
+                  <h2 className="pr-9 font-bold text-x ">Olá, faça seu login!</h2>
                  </div>
                  <Dialog>
                     <DialogTrigger asChild>
                         <Button size="icon">
-                         <LogOutIcon/>
+                         <LogInIcon/>
                         </Button>   
                     </DialogTrigger>
+
+
                     <DialogContent className="w-[90%] rounded-xl">
                         <DialogHeader>
                             <DialogTitle> Faça login na plataforma</DialogTitle>
                             <DialogDescription>Conecte-se usando sua conta Google</DialogDescription>
                         </DialogHeader>
-                        <Button variant="outline" className="gap-2 font-bold">
+                        <Button variant="outline" className="gap-2 font-bold"
+                        onClick={hendleLoginWithGoogleClick}>
                             <FcGoogle size={30}/>
                             Google
                         </Button>
                     </DialogContent>
-                 </Dialog>
+                 </Dialog>   
+
+                </>)}
+
 
             </div>
             
@@ -70,9 +99,12 @@ const SidebarSheet = () => {
                 ))}
             </div>
             <div className="py-5 gap-2 flex flex-col ">
-                <Button className="justify-start gap-2" variant="ghost">
+                <Button className="justify-start gap-2"
+                 variant="ghost"
+                 onClick={handleLogoutClick}>
                     <LogOutIcon size={18} />
-                    Sair da Conta</Button>
+                    Sair da Conta
+                    </Button>
             </div>
 
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "./_components/ui/sonner";
 import Footer from "./_components/footer";
+import AuthProvider from "./_providers/auth";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,9 +19,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body className={inter.className}> {children}
+      <body className={inter.className}>
+        <AuthProvider>
+         {children}
         <Toaster/>
-        <Footer/>
+        <Footer/> 
+        </AuthProvider>
+
       </body>
     </html>
   );
